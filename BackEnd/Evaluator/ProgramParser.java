c10a97d74c9ed5a735308e70d1f457bca243fec1
Oliver Tongrak <tongrak_ru@cmu.ac.th>
2022-02-15 01:50:55 +0700

    ProgramParser redesign commit

--- BackEnd/Evaluator/ProgramParser.java
+++ BackEnd/Evaluator/ProgramParser.java
@@ -1,34 +1,12 @@
 import java.util.LinkedList;
 import java.util.Map;
 
-enum Direction{left, right, up, down, upleft, upright, downleft, downright}
+//enum Direction{left, right, up, down, upleft, upright, downleft, downright}
 
-interface StatementBox{
-//    boolean isTerminable();
-    GPAction getGPAction(); //if return null == not terminable
-}
-
-abstract class BlockStatementAbs implements StatementBox{
-    private LinkedList<StatementBox> statementLL;
-}
-abstract class ifStatementAbs implements StatementBox{
-    private ExprAbs theExpr;
-    private StatementBox thenStatement;
-    private StatementBox elseStatement;
-}
-
-abstract class StatementParser{
-    abstract StatementBox parse(); //Throw Syntax error
-        //while (tk.hasNext)
-        //command parse // if tk.peer is identifier then assignment else it's GPs
-        //if tk.peer == { -> loop parse till detect } -> return BlockStatement
-        //if tk.peer == if -> create ExpressionObj
-        //if 
-
-}
 
 public class ProgramParser {
-    private Map<String, Integer> binding;
+    private Map<String, ExprAbs> binding;
     private LinkedList<StatementBox> statementLL;
+    private final String[] reservedKeyWord = {"antibody", "else", "if", "move", "nearby", "shoot", "then", "virus", "while", "left", "right", "up", "down", "upleft", "upright", "downleft", "downright"};
 
 }
