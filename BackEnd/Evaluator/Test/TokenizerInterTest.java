0d1897b5d31071897f124cbe3a701da8272bd0fe
Oliver Tongrak <tongrak_ru@cmu.ac.th>
2022-02-14 22:07:36 +0700

    Testing foothold commit

---
+++ BackEnd/Evaluator/Test/TokenizerInterTest.java
@@ -0,0 +1,18 @@
+package Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TokenizerInterTest {
+
+    @org.junit.jupiter.api.Test
+    void tokenize() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void peer() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void pop() {
+    }
+}
