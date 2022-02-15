2a6a43fbec2831fa18a7ed328fe69406e60d41cd
Oliver Tongrak <tongrak_ru@cmu.ac.th>
2022-02-15 21:18:50 +0700

    GPsStorage's important method turn static
    plus GPsStorage.coorOfGP, GPsStorage.containGP, GPsStorage.setGPintoStorage implemented

--- BackEnd/Evaluator/GPsStorage.java
+++ BackEnd/Evaluator/GPsStorage.java
@@ -1,70 +1,70 @@
 import java.util.HashMap;
 import java.util.Map;
 
-interface GPsStorageInter{
-    /** return nearest Virus's 8-directional coor from inputted GP.
-     * @return nearest Virus in the form of 8-directional coor
-     * */
-    int nearestVirus(GamePiece gp);
-    /** return nearest AB 8-directional coor from inputted GP.
-     * @return nearest AB in the form of 8-directional coor
-     * */
-    int nearestAntiBody(GamePiece gp);
-    /** return nearest GamePiece's 1-directional GP's coor from inputted GP.
-     * @return nearest GP in given direction. if it a virus return 10x+1 else (AB) 10x+2; x = distance from host GP.
-     * */
-    int nearbyInDirec(int eightDirecCoor);
-
-    /** return true if given Coor is occupied or not
-     *
-     * @return true if said Coor is occupied else false;
-     * */
-    boolean coorOccupied(Coor coor);
-
-    /** check if given GP existed in the storage;
-     * @return true if given GP existed in the storage, else false;
-     * */
-    boolean containGP(GamePiece gp);
-}
-
-public class GPsStorage implements GPsStorageInter {
+public class GPsStorage {
     private GPsStorage instance;
     private GPsStorage(){}
-    private Map<Coor, GamePiece> GPCoorMap;
+    private static Map<Coor, GamePiece> GPCoorMap;
 
     public GPsStorage getInstance(){
         if(this.instance == null){
             this.instance = new GPsStorage();
-            this.GPCoorMap = new HashMap<>();
+            GPCoorMap = new HashMap<>();
         }
         return this.instance;
     }
 
-
-    @Override
-    public int nearestVirus(GamePiece gp) {
-        return 0;
+    /** return nearest Virus's 8-directional coor from inputted GP.
+     * @return nearest Virus in the form of 8-directional coor
+     * */
+    public static int nearestVirus(GamePiece gp) {
+        throw new RuntimeException("unimplemented");
+    }
+    /** return nearest AB 8-directional coor from inputted GP.
+     * @return nearest AB in the form of 8-directional coor
+     * */
+    public static int nearestAntiBody(GamePiece gp) {
+        throw new RuntimeException("unimplemented");
+    }
+    /** return nearest GamePiece's 1-directional GP's coor from inputted GP.
+     * @return nearest GP in given direction. if it a virus return 10x+1 else (AB) 10x+2; x = distance from host GP.
+     * */
+    public static int nearbyInDirec(int eightDirecCoor) {
+        throw new RuntimeException("unimplemented");
+    }
+    /** return true if given Coor is occupied or not
+     * @return true if said Coor is occupied else false;
+     * */
+    public static boolean coorOccupied(Coor coor) {
+        return !GPCoorMap.containsKey(coor);
+    }
+    /** check if given GP existed in the storage;
+     * @return true if given GP existed in the storage, else false;
+     * */
+    public static boolean containGP(GamePiece gp) {
+        return GPCoorMap.containsValue(gp);
     }
 
-    @Override
-    public int nearestAntiBody(GamePiece gp) {
-        return 0;
+    public static Coor coorOfGP(GamePiece gp) {
+        if(!containGP(gp))return null;
+        for(Coor c: GPCoorMap.keySet())if(gp.equals(GPCoorMap.get(c))) return c;
+        return null;
     }
 
-    @Override
-    public int nearbyInDirec(int eightDirecCoor) {
-        return 0;
+    /** adding or change already exist gp current coor iff given coor is not occupied;
+     *
+     * @return true if adding or change was successful;
+     * */
+    public static boolean setGPintoStorage(GamePiece gp, Coor coor){
+        if(!containGP(gp)){
+            if(!coorOccupied(coor)) { GPCoorMap.put(coor,gp); return true;}
+            else return false;
+        }else{
+            if(!coorOccupied(coor)){
+                GPCoorMap.remove(coorOfGP(gp));
+                GPCoorMap.put(coor,gp);
+                return true;
+            }else return false;
+        }
     }
-
-    @Override
-    public boolean coorOccupied(Coor coor) {
-        return false;
-    }
-
-    @Override
-    public boolean containGP(GamePiece gp) {
-        return false;
-    }
-
-
 }
