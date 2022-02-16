da86cc5ee790eef5c9f94bd6dbc33f840cc4f50e
Oliver Tongrak <tongrak_ru@cmu.ac.th>
2022-02-16 15:44:42 +0700

    ProgramParser foothold

--- BackEnd/Evaluator/ProgramParser.java
+++ BackEnd/Evaluator/ProgramParser.java
@@ -1,3 +1,4 @@
+import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.Map;
 
@@ -5,8 +6,27 @@ import java.util.Map;
 
 
 public class ProgramParser {
-    private Map<String, ExprAbs> binding;
-    private LinkedList<StatementBox> statementLL;
+    protected final Map<String, ExprAbs> binding;
+    protected final LinkedList<StatementBox> statementLL;
     private final String[] reservedKeyWord = {"antibody", "else", "if", "move", "nearby", "shoot", "then", "virus", "while", "left", "right", "up", "down", "upleft", "upright", "downleft", "downright"};
 
+    StatementParser sParer;
+    ExpressionParser eParser;
+
+    public ProgramParser(Tokenizer tk, Coor theCoor) throws SyntaxError {
+        this.binding = new HashMap<>();
+        this.statementLL = new LinkedList<>();
+
+        eParser = new ExpressionParser(tk, binding, theCoor);
+        sParer = new StatementParser(tk, reservedKeyWord, binding, eParser);
+
+        StatementBox currStatement = null;
+        while(tk.peer()!=null){
+            currStatement = sParer.parseS();
+        }
+    }
+
+    public LinkedList<StatementBox> getStatementLL() {
+        return statementLL;
+    }
 }
