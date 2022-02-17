d5b45cb52635bea01ef58629b5ea5de75656a164
Oliver Tongrak <tongrak_ru@cmu.ac.th>
2022-02-18 02:12:36 +0700

    GameMediator interface foothold

--- BackEnd/carin/src/main/java/yukongold/carin/requesthandler/GameMediator.java
+++ BackEnd/carin/src/main/java/yukongold/carin/requesthandler/GameMediator.java
@@ -3,6 +3,65 @@ package yukongold.carin.requesthandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**As the name might suggest, the "class" handle when player request game to 
+ * start or manipulate game's speed (pause, slow, fast)
+ */
+interface RequestHandler{
+
+    /**a order to initiate the game and let player in spawning phases
+     * 
+     * @return reply confirm the order
+     */
+    RequestHandler start();
+
+    /**a order to kick start the game properly.
+     * 
+     * @return reply confirm the order
+     */
+    RequestHandler startGame();
+
+    /**a order to pass game's data
+     * 
+     * @return json file represent all GP and its coor, plus current game speed.
+     */
+    RequestHandler update();
+
+    /**a order to change game's speed
+     * 
+     * @return reply confirm the order
+     */
+    RequestHandler speedAct();
+
+    /**a order to pause the game;
+     * 
+     * @return reply confirm the order
+     */
+    RequestHandler pauseAct();
+
+    /**a order to change antibody coor
+     * 
+     * @return reply confirm the order
+     */
+    // RequestHandler relocateAct();
+
+    /**a order to create a new antibody at coor passed
+     * 
+     * @return reply confirm the order
+     */
+    // RequestHandler spawnAct();
+
+    /**a order to selecte a certain ClickSensitiveComponent
+     * 
+     * @return reply confirm the order
+     */
+    RequestHandler clickAct();
+}
+
+abstract class RequestHandlerAbs implements RequestHandler{
+    // GameBoards instance
+
+} 
+
 @RestController
 public class GameMediator {
 
