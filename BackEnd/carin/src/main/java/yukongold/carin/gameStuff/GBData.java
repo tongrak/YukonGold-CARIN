e34b77d04b3ad1dd3ef6633f40b54356f326b378
Oliver Tongrak <tongrak_ru@cmu.ac.th>
2022-02-18 23:43:58 +0700

    GBData basics implement

--- BackEnd/carin/src/main/java/yukongold/carin/gameStuff/GBData.java
+++ BackEnd/carin/src/main/java/yukongold/carin/gameStuff/GBData.java
@@ -3,7 +3,54 @@ package yukongold.carin.gamestuff;
 import java.util.Map;
 
 public class GBData {
-    boolean isPause;
-    int currSpeed;
-    Map<Coor, GamePiece> data;
+    private boolean clickPause;
+    private boolean clickSpeed;
+    private int currSpeed;
+    private PlayerAction currRequest;
+    private static Map<Coor, GamePiece> GPCoorMap;;
+    private static GBData instance;
+    private GBData(){}
+
+    public static GBData getInstance(){
+        if(instance == null){
+            instance = new GBData();
+            GPsStorage storage = GPsStorage.getInstance();
+            GPCoorMap = storage.getMap();
+        }
+        return instance;
+    }
+
+    public PlayerAction getCurrRequest(){
+        PlayerAction holder = null;
+        if(currRequest != null){ holder = currRequest; currRequest = null;}
+        return holder;
+    }
+
+    public void setClickPause(){
+        this.clickPause = true;
+    }
+
+    public boolean isPlClickPause(){
+        if(clickPause){
+            this.clickPause = false;
+            return true;
+        }else return false;
+    }
+
+    public void setClickSpeed(){
+        this.clickSpeed = true;
+    }
+
+    public boolean isPlClickSpeed(){
+        if(clickSpeed){
+            this.clickSpeed = false;
+            return true;
+        }else return false;
+    }
+
+
+
+    
+
+
 }
