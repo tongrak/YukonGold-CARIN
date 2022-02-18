d212b5ac2bf405d658cb7d186670f1778815677e
Oliver Tongrak <tongrak_ru@cmu.ac.th>
2022-02-18 23:49:24 +0700

    GPsStrorage change to instance  && method added

--- BackEnd/carin/src/main/java/yukongold/carin/gameStuff/GPsStorage.java
+++ BackEnd/carin/src/main/java/yukongold/carin/gameStuff/GPsStorage.java
@@ -1,19 +1,24 @@
 package yukongold.carin.gamestuff;
 
+import java.nio.file.Path;
 import java.util.HashMap;
 import java.util.Map;
 
 public class GPsStorage {
-    private GPsStorage instance;
+    private static GPsStorage instance;
     private GPsStorage(){}
     private static Map<Coor, GamePiece> GPCoorMap;
 
-    public GPsStorage getInstance(){
-        if(this.instance == null){
-            this.instance = new GPsStorage();
+    public static GPsStorage getInstance(){
+        if(instance == null){
+            instance = new GPsStorage();
             GPCoorMap = new HashMap<>();
         }
-        return this.instance;
+        return instance;
+    }
+
+    public Map<Coor, GamePiece> getMap(){
+        return GPCoorMap;
     }
 
     /** return nearest Virus's 8-directional coor from inputted GP.
@@ -57,7 +62,7 @@ public class GPsStorage {
      *
      * @return true if adding or change was successful;
      * */
-    public static boolean setGPintoStorage(GamePiece gp, Coor coor){
+    public boolean setGPintoStorage(GamePiece gp, Coor coor){
         if(!containGP(gp)){
             if(!coorOccupied(coor)) { GPCoorMap.put(coor,gp); return true;}
             else return false;
@@ -68,5 +73,38 @@ public class GPsStorage {
                 return true;
             }else return false;
         }
+    }
+
+    public boolean relocateAB(Coor selected, Coor destination){
+        return setGPintoStorage(GPCoorMap.get(selected), destination);
+    }
+
+    /**return true if current winning condition is met, Which is all GP on the boards consist only one side (AB or Virus) 
+     * 
+     * @return true if all GP on the boards consist only one side.
+     */
+    public boolean checkWinningCond(){
+        int GPDiff = 0;
+        if(GPCoorMap.keySet().size() == 0) return true;
+        for(Coor c: GPCoorMap.keySet()){
+            if(GPCoorMap.get(c).getClass().equals(Antibody.class)){
+                GPDiff++;
+            }else{
+                GPDiff--;
+            }
+        }
+        return Math.abs(GPDiff) == GPCoorMap.keySet().size();
+    }
+
+    public boolean removeGP(GamePiece gp){
+        throw new RuntimeException("Unimplemented");
+    }
+
+    /**replace AB with new Virus at the same coor
+     * 
+     * @return true if replacment was succuessful.
+     */
+    public boolean replaceABwithVirus(Path virusGene, Coor ABCoor){
+        throw new RuntimeException("Unimplemented");
     }
 }
