947bb9f42ff86ad6c7b8613eaa0d8aa07888b7e8
Oliver Tongrak <tongrak_ru@cmu.ac.th>
2022-02-19 01:16:37 +0700

    QuickTest added

---
+++ BackEnd/carin/src/test/java/yukongold/carin/QuickTest.java
@@ -0,0 +1,18 @@
+package yukongold.carin;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+import yukongold.carin.gamestuff.Coor;
+
+public class QuickTest {
+
+    @Test
+    void theTest(){
+        Coor c1 = new Coor(0,0);
+        Coor c2 = new Coor(0,0);
+        assertEquals(c1, c2);
+    }
+    
+}
