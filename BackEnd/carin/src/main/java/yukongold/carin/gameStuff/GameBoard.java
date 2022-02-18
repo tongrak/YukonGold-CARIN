6e4fbb40f196099a139d417e6785c4001d7acb28
Oliver Tongrak <tongrak_ru@cmu.ac.th>
2022-02-18 23:42:48 +0700

    GameBoards basics implement

--- BackEnd/carin/src/main/java/yukongold/carin/gameStuff/GameBoard.java
+++ BackEnd/carin/src/main/java/yukongold/carin/gameStuff/GameBoard.java
@@ -1,14 +1,72 @@
 package yukongold.carin.gamestuff;
 
-interface carinRep{
-    carinRep getInstance();
-    void init();
-    void pauseGame();
-    void speedChange();
-    void twoClickAct();
-    GBData getData();
-}
+public class GameBoard{
+    private static GameBoard instance;
+    private static GPsFactory GPsFac;
+    private static GPsPlayer GPsPlay;
+    private static GPsStorage GPsStore;
+    private GBData theData;
+    private GameBoard(){}
 
-public class GameBoard {
+    public static GameBoard getInstance(){
+        if(instance == null){
+            instance = new GameBoard();
+            GPsFac = GPsFactory.getInstance();
+            GPsPlay = GPsPlayer.getInstance();
+            GPsStore = GPsStorage.getInstance();
+        }
+        return instance;
+    }
+
+
+    public void startGame(){
+        boolean metWinningCond = false;
+        while(!metWinningCond){
+            checkPlayerRequest();
+            GPsPlay.startGPsTurn();
+            metWinningCond = GPsStore.checkWinningCond();
+        }
+    }
+
+    private void checkPlayerRequest(){
+        PlayerAction holder = theData.getCurrRequest();
+        if(holder !=  null){
+            switch (holder.getClass().getName()) {
+                case "SpawnAct"->{
+                    SpawnAct spawnHolder = (SpawnAct) holder;
+                    Antibody newAB = GPsFac.createNewAB(spawnHolder.getSelectedABType());
+                    boolean check = GPsStore.setGPintoStorage(newAB, spawnHolder.getSelectedCoor());
+                    if(!check) throw new RuntimeException("SpawnAct: can't spawn "); else GPsPlay.addGP(newAB);
+                }
+                case "RelocateAct"->{
+                    RelocateAct relocateHolder = (RelocateAct) holder;
+                    boolean check = GPsStore.relocateAB(relocateHolder.getSelectedCoor(), relocateHolder.getDestination());
+                    if(!check) throw new RuntimeException("RelocateAct: can't relocated");
+                }
+            }
+        }
+    }
+
+    public GBData init() {
+        // TODO Auto-generated method stub
+        return theData;
+        
+    }
+
+    public void pauseGame() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    public void speedChange() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    public GBData updateData() {
+        // TODO Auto-generated method stub
+        return null;
+    }
     
 }
+ 
