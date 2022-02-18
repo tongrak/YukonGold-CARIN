c5c0fa6a31626c13bfda3ba0ee2e452489ba004a
Oliver Tongrak <tongrak_ru@cmu.ac.th>
2022-02-18 23:48:33 +0700

    GPsPlayer basics Implement

--- BackEnd/carin/src/main/java/yukongold/carin/gameStuff/GPsPlayer.java
+++ BackEnd/carin/src/main/java/yukongold/carin/gameStuff/GPsPlayer.java
@@ -2,16 +2,38 @@ package yukongold.carin.gamestuff;
 
 import java.util.LinkedList;
 
-interface GPsPlayerInter{
-    void turnStart();
-    void addGP(GamePiece gp);
-    void removeGP(GamePiece gp);
-}
-
-abstract class GPsPlayerAbs{
-    LinkedList<GamePiece> GPlist;
-}
-
 public class GPsPlayer {
-    
+    private int gpCount = 0;
+    private static LinkedList<GamePiece> GPLL;
+    private static GPsPlayer instance;
+    private GPsPlayer(){}
+
+    public static GPsPlayer getInstance(){
+        if(instance == null){
+            instance = new GPsPlayer();
+            GPLL = new LinkedList<>();
+        }
+        return instance;
+    }
+
+    public void startGPsTurn(){
+        updateGPcount();
+        for(int i = 0; i < gpCount; i++){
+            GPLL.get(i).turnStart();
+        }
+    }
+
+    public void addGP(GamePiece gp){
+        GPLL.add(gp);
+    }
+
+    public void removeGP(GamePiece gp){
+        GPLL.remove(gp);
+        gpCount--;
+    }
+
+    public void updateGPcount(){
+        this.gpCount = GPLL.size();
+    }
+
 }
