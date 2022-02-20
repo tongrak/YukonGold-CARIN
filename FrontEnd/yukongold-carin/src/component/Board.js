8c4f622ca08d222114657a5496174ad30fdb2ad5
Nadtaphong Jandaboot <man2545_@hotmail.com>
2022-02-21 00:19:49 +0700

    GameplayCellComponent#1

---
+++ FrontEnd/yukongold-carin/src/component/Board.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Board.css';
+
+const VerticalAxis = ["1","2","3","4","5","6","7","8"];
+const HorizontalAxis = ["a","b","c","d","e","f","g","h"];
+
+export default function Board(){
+    let board = [];
+    for(let i = VerticalAxis.length-1;i >= 0;i--){
+        for(let j = 0;j < HorizontalAxis.length;j++){
+            board.push(
+            <div className = "tile"></div>)
+        }
+    }
+    return <div id="Board">{board}</div>
+}
