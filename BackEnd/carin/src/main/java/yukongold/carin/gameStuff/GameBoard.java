188e261bd7148f61802e18f76d2421b2492b4569
Oliver Tongrak <tongrak_ru@cmu.ac.th>
2022-02-22 02:56:24 +0700

    GB wait basic implement

--- BackEnd/carin/src/main/java/yukongold/carin/gameStuff/GameBoard.java
+++ BackEnd/carin/src/main/java/yukongold/carin/gameStuff/GameBoard.java
@@ -5,77 +5,91 @@ import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
 //TODO Testing
-//TODO creat a visual class to run the game on terminal
 
+public class GameBoard {
 
-public class GameBoard{
-
-    class MainLoop implements Runnable{
-
-        @Override
-        public void run() {
-            mainLoop();
-        }
-    
-    }
+    // class MainLoop extends Thread{
+    // @Override
+    // public void run() {
+    // mainLoop();
+    // }
+    // }
     private int speedIndex = 0;
-    private Long[] speedRate = {1L, 2L, 3L};
+    private int turnCounter = 0;
+    private boolean metWinningCond = false;
+    private int[] speedRate = { 1, 2, 3 };
     private boolean isPause = false;
     private static GameBoard instance;
-    private GPsFactory GPsFac;
-    private GPsPlayer GPsPlay;
-    private GPsStorage GPsStore;
-    private GBData theData;
-    private GameBoard(){}
+    private static GPsFactory GPsFac;
+    private static GPsPlayer GPsPlay;
+    private static GPsStorage GPsStore;
+    private static GBData theData;
 
-    public static GameBoard getInstance(){
-        if(instance == null){
+    private GameBoard() {
+    }
+
+    public static GameBoard getInstance() {
+        if (instance == null) {
+            GPsFac = GPsFactory.getInstance();
+            GPsPlay = GPsPlayer.getInstance();
+            GPsStore = GPsStorage.getInstance();
+            theData = GBData.getInstance();
             instance = new GameBoard();
         }
         return instance;
     }
 
-    public void start(){
+    public void start() {
         final ScheduledExecutorService executorService = Executors.newSingleThreadScheduledExecutor();
-        final MainLoop ml = new MainLoop();
-        executorService.scheduleAtFixedRate(ml, 0, speedRate[speedIndex], TimeUnit.SECONDS);
-    }
-
-    private void mainLoop(){
-        boolean metWinningCond = false;
-        while(!metWinningCond){
-            checkPlayerRequest();
-            GPsPlay.startGPsTurn();
-            metWinningCond = GPsStore.checkWinningCond();
-        }
-    }
-
-    private void checkPlayerRequest(){
-        PlayerAction holder = theData.getCurrRequest();
-        if(holder !=  null){
-            switch (holder.getClass().getName()) {
-                case "SpawnAct"->{
-                    SpawnAct spawnHolder = (SpawnAct) holder;
-                    Antibody newAB = GPsFac.createNewAB(spawnHolder.getSelectedABType());
-                    // TODO get config data
-                    boolean check = GPsStore.setGPintoStorage(newAB, spawnHolder.getSelectedCoor());
-                    if(!check) throw new RuntimeException("SpawnAct: can't spawn "); else GPsPlay.addGP(newAB);
-                }
-                case "RelocateAct"->{
-                    RelocateAct relocateHolder = (RelocateAct) holder;
-                    boolean check = GPsStore.relocateAB(relocateHolder.getSelectedCoor(), relocateHolder.getDestination());
-                    if(!check) throw new RuntimeException("RelocateAct: can't relocated");
-                }
+        // final MainLoop ml = new MainLoop();
+        // ml.setDaemon(true);
+        // executorService.scheduleAtFixedRate(ml, 0, speedRate[speedIndex],
+        // TimeUnit.SECONDS);
+        
+        while (true) {
+            mainLoop();
+            try {
+                Thread.sleep(speedRate[speedIndex]*1000);
+            } catch (InterruptedException ex) {
+                Thread.currentThread().interrupt();
             }
         }
     }
 
-    public void init() {
-        GPsFac = GPsFactory.getInstance();
-        GPsPlay = GPsPlayer.getInstance();
-        GPsStore = GPsStorage.getInstance();
-        theData = GBData.getInstance();
-        
+    private void mainLoop() {
+        boolean metWinningCond = false;
+        if (!metWinningCond) {
+            checkPlayerRequest();
+            if (!isPause)
+                GPsPlay.startGPsTurn();
+            metWinningCond = GPsStore.checkWinningCond();
+            System.out.println("current win con: " + metWinningCond);
+            System.out.println("GB: is " + ((isPause) ? "pause" : "running") + " at turn:"
+                    + ((isPause) ? turnCounter : turnCounter++));
+        }
+    }
+
+    private void checkPlayerRequest() {
+        PlayerAction holder = theData.getCurrRequest();
+        if (holder != null) {
+            if(holder.getClass().equals(SpawnAct.class)){
+                SpawnAct spawnHolder = (SpawnAct) holder;
+                Antibody newAB = GPsFac.createNewAB(spawnHolder.getSelectedABType());
+                // TODO get config data
+                boolean check = GPsStore.setGPintoStorage(newAB, spawnHolder.getSelectedCoor());
+                if (!check)
+                    throw new RuntimeException("SpawnAct: can't spawn ");
+                else
+                    GPsPlay.addGP(newAB);
+
+            }else{
+                RelocateAct relocateHolder = (RelocateAct) holder;
+                    boolean check = GPsStore.relocateAB(relocateHolder.getSelectedCoor(),
+                            relocateHolder.getDestination());
+                    if (!check)
+                        throw new RuntimeException("RelocateAct: can't relocated");
+            }
+        }
     }
 
     public void pauseGame() {
@@ -84,11 +98,8 @@ public class GameBoard{
 
     public void speedChange() {
         this.speedIndex++;
-        if(speedIndex>3) this.speedIndex = 0;
+        if (speedIndex > 3)
+            this.speedIndex = 0;
     }
 
-    // public GBData updateData() {
-    // }
-    
 }
- 
