496bf51431e90871e874e3f1193476978c5acc9b
Oliver Tongrak <tongrak_ru@cmu.ac.th>
2022-02-22 00:48:33 +0700

    GBData Document added

--- BackEnd/carin/src/main/java/yukongold/carin/gameStuff/GBData.java
+++ BackEnd/carin/src/main/java/yukongold/carin/gameStuff/GBData.java
@@ -20,16 +20,26 @@ public class GBData {
         return instance;
     }
 
+    /** Design for GameBoard(Mainloop) to access current player request
+     * 
+     */
     public PlayerAction getCurrRequest(){
         PlayerAction holder = null;
         if(currRequest != null){ holder = currRequest; currRequest = null;}
         return holder;
     }
 
+    /** Design for GameMediator to pass on player requested for pause
+     * 
+     */
     public void setClickPause(){
         this.clickPause = true;
     }
 
+    /**Design for GameBoard(Mainloop) to check if player request for pause or not
+     * 
+     * @return true if player request for pause. else false.
+     */
     public boolean isPlClickPause(){
         if(clickPause){
             this.clickPause = false;
@@ -37,10 +47,17 @@ public class GBData {
         }else return false;
     }
 
+    /**Design for GameMediator to pass player request on game's speed
+     * 
+     */
     public void setClickSpeed(){
         this.clickSpeed = true;
     }
 
+    /**Design for GameBoard(MainLoop) to check if player request change gamespeed or not
+     * 
+     * @return true if player requested for game speed change 
+     */
     public boolean isPlClickSpeed(){
         if(clickSpeed){
             this.clickSpeed = false;
@@ -48,17 +65,20 @@ public class GBData {
         }else return false;
     }
 
+
+    /** getting Map object of current game data
+     * 
+     * @return a Map object represent a game data.
+     */
     public Map<Coor, GamePiece> getMappingData(){
         return GPCoorMap;
     }
 
+    /**Design for GameMediator to pass on player requested action(spawn / relocate)
+     * 
+     * @param PlAct - PlayerAction which had been analize of GameMediator
+     */
     public void setPlayerAction(PlayerAction PlAct){
         if(currRequest == null){currRequest = PlAct;}
     }
-
-
-
-    
-
-
 }
