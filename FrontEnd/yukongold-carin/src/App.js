0b4da50ea1654ac6d537bc8f6ab7b1e05f8a5988
Kittiphum <76035271+kittiphum717@users.noreply.github.com>
2022-02-23 01:42:44 +0700

    Test tailwind css

--- FrontEnd/yukongold-carin/src/App.js
+++ FrontEnd/yukongold-carin/src/App.js
@@ -1,14 +1,27 @@
 import Board from './component/Board/Board';
 import Menu from './component/Menu';
+import Title from './component/title/Title';
 import './App.css';
 
 function App() {
   return (
-    <div id="app">
-      <h1 className='text-sky-400/100'>Hello ViRuS</h1>
+    
+    <div id="app" className='bg-white'>
+
+
+      <div className=''>
+        <div className='pt-10 pb-6 px-72'>
+          <Title/>
+        </div>
+          
+        <div className='pt-10 flex px-10'>
+          <Menu/>
+          <Board/>
+        </div>
+        
+      </div>
       
-      <Menu/>
-      <Board/>
+      
         
     </div>
   );
