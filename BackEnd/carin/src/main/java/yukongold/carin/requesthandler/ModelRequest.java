f4d06a004b15483820abec635e001a4e3b8ab6ca
Kittiphum <76035271+kittiphum717@users.noreply.github.com>
2022-02-22 15:03:58 +0700

    Implement PostMapping(Click)

---
+++ BackEnd/carin/src/main/java/yukongold/carin/requesthandler/ModelRequest.java
@@ -0,0 +1,19 @@
+package yukongold.carin.requesthandler;
+
+import yukongold.carin.gamestuff.Coor;
+import yukongold.carin.gamestuff.PlayerActAbs;
+
+public class ModelRequest {
+    
+    private int id;
+
+    private String coor;
+
+    public int getID(){
+        return this.id;
+    }
+
+    public String getCoor(){
+        return this.coor;
+    }
+}
