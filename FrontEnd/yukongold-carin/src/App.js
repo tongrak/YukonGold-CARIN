086793d2dbe8d0a5a064e765e7b2616bdf4703f3
Kittiphum <76035271+kittiphum717@users.noreply.github.com>
2022-02-24 15:06:48 +0700

    Implement show & hide tage input and button

--- FrontEnd/yukongold-carin/src/App.js
+++ FrontEnd/yukongold-carin/src/App.js
@@ -2,51 +2,74 @@ import Board from './component/Board/Board';
 import Menu from './component/Menu';
 import Title from './component/title/Title';
 import './App.css';
-import { BrowserRouter as Router, Switch,Route,Link} from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import { useState } from 'react';
+import axios from 'axios';
 
 function App() {
 
-  const [inputData,setInputdata] = useState(0)
-  const [grid,setGrid] = useState(0)
+  const [inputData, setInputdata] = useState(0)
+  const [grid, setGrid] = useState(0)
+  const [buttonDisabled, setButtondisable] = useState(true)
+  const [inputDisabled, setInputDisable] = useState(true)
 
+  const start = () => {
+    axios.get("http://localhost:8080/start")
+      .then(res => {
+        console.log(res.data)
+      })
+  }
 
-  const inputOnchange = (e) =>{
+  const inputOnchange = (e) => {
     setInputdata(e.target.value)
     console.log(inputData)
-    
   }
 
   const createGrid = () => {
     setGrid(inputData)
     console.log(inputData)
+    start();
+    setButtondisable(false)
+    setInputDisable(false)
   }
 
 
   return (
-    
+
     <div className='bg-white'>
 
       <div className='flex justify-center pt-10 pb-5 '>
-        <Title/>
+        <Title />
       </div>
 
       <div className='flex justify-center'>
-        <input type="number" className='border-2 rounded-md p-1 placeholder:italic placeholder:text-slate-400' 
-        placeholder="input M x N" onChange={inputOnchange}></input>
+        {inputDisabled ?
+          <input
+            type="number"
+            className='border-2 rounded-md p-1 placeholder:italic placeholder:text-slate-400'
+            placeholder="input m x n"
+            onChange={inputOnchange}
+          >
+          </input> : null}
       </div>
 
       <div className='flex justify-center pt-3'>
-        <button className='border-2 border-lime-500 border-double rounded px-5'
-        onClick={createGrid}>Start</button>
-      </div>
-      
+        {buttonDisabled ?
+          <button
+            className='border-2 border-lime-500 border-double rounded px-5'
+            onClick={createGrid}
+          >
+            Start
+          </button> : null}
 
-      <div className='flex justify-center pt-7 '>
-        <Menu/>
-        <Board layout={grid}/>
       </div>
-        
+
+
+      <div className='flex justify-center pt-7'>
+        <Menu />
+        <Board layout={grid} />
+      </div>
+
     </div>
   );
 }
